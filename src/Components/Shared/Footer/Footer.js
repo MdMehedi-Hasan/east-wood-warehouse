872e75092fa557d5126ece2ee4a77e19d8439baf
MdMehedi-Hasan <mh19115@gmail.com>
2022-05-09 09:16:43 +0600

    footer responsive done

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -2,20 +2,21 @@ import { Icon } from "@iconify/react";
 import React from "react";
 import playStore from '../../../images/g-store.png'
 import appStore from '../../../images/app-store.png'
+import './footer.css'
 
 const Footer = () => {
   return (
     <div className="text-white bg-dark p-4 container-fluid">
       <div className="row">
-        <div className="col-md-6 col-sm-12 col-lg-4">
+        <div className="col-md-6 col-sm-12 col-lg-4 text-center text-lg-start">
        <h1 >East<span className="text-green">wood</span></h1> 
        <h5 className="mt-5">Find more</h5>
-       <div className="bg-white w-75 rounded-pill ">
+       <div className="bg-white w-75 rounded-pill mx-auto mx-lg-0">
        <input style={{outline:'none'}} className="border border-0 w-75 rounded-pill text-center" type="text" />
        <button className="text-dark border-0 w-25 rounded-pill py-2">Search</button>
        </div>
         </div>
-        <div className="col-md-6 col-sm-6 col-lg-2">
+        <div className="col-sm-6 col-md-6 col-lg-2">
             <h4>Useful links</h4>
             <ul className="list-unstyled">
             <li>Home</li>          
@@ -37,10 +38,10 @@ const Footer = () => {
         </div>
         <div className="col-md-6 col-sm-12 col-lg-2">
                   <h4 className="text-center">Contact Us</h4> 
-                  <ul className="d-flex list-unstyled justify-content-around">
-                      <li><Icon className="fs-3" icon="akar-icons:google-fill" /></li>
-                      <li><Icon className="fs-3" icon="bi:facebook" /></li>
-                      <li><Icon className="fs-3" icon="cib:twitter" /></li>
+                  <ul className="d-flex list-unstyled justify-content-lg-around justify-content-center">
+                      <li className="me-3 me-lg-0"><Icon className="fs-3" icon="akar-icons:google-fill" /></li>
+                      <li className="me-3 me-lg-0"><Icon className="fs-3" icon="bi:facebook" /></li>
+                      <li className="me-3 me-lg-0"><Icon className="fs-3" icon="cib:twitter" /></li>
                   </ul>
             </div>
         <div className="col-md-6 col-sm-12 col-lg-2">
